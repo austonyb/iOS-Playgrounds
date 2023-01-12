4f3011aec3481ad8f3d1a9e74eef83f35020f5f9
Auston Youngblood <95832900+austonyb@users.noreply.github.com>
2023-01-11 18:36:12 -0700

    more challenges

--- closures.playground/Contents.swift
+++ closures.playground/Contents.swift
@@ -1,5 +1,18 @@
 import Cocoa
 
+//So, if the first name is Suzanne, return true so that name1 is sorted before name2. On the other hand, if name2 is Suzanne, return false so that name1 is sorted after name2. If neither name is Suzanne, just use < to do a normal alphabetical sort.
+
 let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
 
+func captainFirstSorted(name1: String, name2: String) -> Bool {
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    
+    return name1 < name2
+}
 
+let captainFirstTeam = team.sorted(by: captainFirstSorted)
+print(captainFirstTeam)
